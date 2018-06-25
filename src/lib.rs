fa338ebdd889a2722f45eef1ba60059a9d142adc
Wenping Guo <ybyygu@gmail.com>
2018-06-25 09:22:00 +0800

    parse model results from string

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // [[file:~/Workspace/Programming/gosh/gosh.note::aab14c1c-34e6-4e2d-b87a-77139ff545df][aab14c1c-34e6-4e2d-b87a-77139ff545df]]
-#[macro_use]
-extern crate quicli;
-mod models;
+#[macro_use] extern crate quicli;
+#[macro_use] extern crate approx;
+
+pub mod models;
 // aab14c1c-34e6-4e2d-b87a-77139ff545df ends here
--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,7 @@
 // [[file:~/Workspace/Programming/gosh/gosh.note::894d0c1b-0482-46b9-a3dc-8f00b78833bc][894d0c1b-0482-46b9-a3dc-8f00b78833bc]]
 use quicli::prelude::*;
 
+#[derive(Debug, Clone)]
 pub struct ModelResults {
     energy          : Option<f64>,
     forces          : Option<Vec<[f64; 3]>>,
@@ -48,5 +49,74 @@ pub trait ChemicalModel {
 // 894d0c1b-0482-46b9-a3dc-8f00b78833bc ends here
 
 // [[file:~/Workspace/Programming/gosh/gosh.note::ea1864bb-6cc4-42f1-93f5-cebd790c58ab][ea1864bb-6cc4-42f1-93f5-cebd790c58ab]]
+use std::str::FromStr;
 
+impl FromStr for ModelResults {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        parse_model_results(s)
+    }
+}
+
+fn parse_model_results(stream: &str) -> Result<ModelResults>{
+    let mut results = ModelResults::default();
+
+    let mut lines = stream.lines();
+    loop {
+        if let Some(line) = lines.next() {
+            if line.starts_with("@") {
+                let header = line.split_whitespace().next();
+                match header {
+                    Some("@total_energy") => {
+                        if let Some(line) = lines.next() {
+                            let energy = line
+                                .trim()
+                                .parse()?;
+                            results.energy = Some(energy);
+                        } else {
+                            warn!("expected energy record not found!");
+                            break;
+                        }
+                    },
+                    Some("@forces") => {
+                        // unimplemented!()
+                    },
+                    Some("@dipole_moments") => {
+                        // unimplemented!()
+                    },
+                    _ => {
+                        warn!("ignored header: {:?}", header);
+                    }
+                }
+            }
+        } else {
+            // file end
+            break;
+        }
+    }
+
+    Ok(results)
+}
+
+#[test]
+fn test_model_parse_results() {
+    let output = "@result_file_format_version
+0.1
+@number_of_atoms I 1
+3
+@total_energy        R 1
+ -0.32933619218901E+00
+@dipole_moments      R 3
+  0.00000000000000E+00  0.00000000000000E+00  0.00000000000000E+00
+@forces              R 9
+ -0.10525500903260E-03 -0.49341715067988E-04 -0.41578514351576E-04
+ -0.10538090864721E-03 -0.44152035166424E-04 -0.46770596426924E-04
+  0.11741769879568E-03 -0.22970049507365E-03 -0.64344681036487E-04
+";
+
+    let r = parse_model_results(output).expect("model results");
+    let e = r.energy.expect("model result: energy");
+    assert_relative_eq!(-0.329336, e, epsilon=1e-4);
+}
 // ea1864bb-6cc4-42f1-93f5-cebd790c58ab ends here
