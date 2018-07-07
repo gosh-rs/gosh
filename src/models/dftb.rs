dac3356a7c4b4440b8e33a5c72a0cc0aeddaa268
Wenping Guo <ybyygu@gmail.com>
2018-07-07 17:00:57 +0800

    update dftb model

--- src/models/dftb.rs
+++ src/models/dftb.rs
@@ -1,6 +1,18 @@
 // [[file:~/Workspace/Programming/gosh/gosh.note::fdd244b4-5403-411a-8cc9-d3782769762e][fdd244b4-5403-411a-8cc9-d3782769762e]]
 use super::*;
 
+pub struct DftbModel {
+    runfile: PathBuf,
+}
+
+impl Default for DftbModel {
+    fn default() -> Self {
+        DftbModel {
+            runfile: "./submit.sh".into(),
+        }
+    }
+}
+
 // perform dftb+ calculations
 pub fn run<P: Into<PathBuf>>(mol: &Molecule, runfile: P) -> Result<ModelResults> {
     let runfile = runfile.into();
@@ -22,5 +34,11 @@ pub fn run<P: Into<PathBuf>>(mol: &Molecule, runfile: P) -> Result<ModelResults>
     let x: ModelResults = output.parse()?;
 
     Ok(x)
+}
+
+impl ChemicalModel for DftbModel {
+    fn calculate(&self, mol: &Molecule) -> Result<ModelResults> {
+        run(&mol, &self.runfile)
+    }
 }
 // fdd244b4-5403-411a-8cc9-d3782769762e ends here
