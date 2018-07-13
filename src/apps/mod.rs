42d974de039b758b9636e4a7f0c2cba3b0911996
Wenping Guo <ybyygu@gmail.com>
2018-07-13 10:06:28 +0800

    add optimizer trait

--- src/apps/mod.rs
+++ src/apps/mod.rs
@@ -1,5 +1,10 @@
 // [[file:~/Workspace/Programming/gosh/gosh.note::fb19e884-ed83-4ad3-a9bf-6a31bb0ad456][fb19e884-ed83-4ad3-a9bf-6a31bb0ad456]]
 use quicli::prelude::*;
+use models::{
+    ChemicalModel,
+    ModelResults,
+};
 
+// sub modules
 pub mod optimization;
 // fb19e884-ed83-4ad3-a9bf-6a31bb0ad456 ends here
--- src/apps/optimization/mod.rs
+++ src/apps/optimization/mod.rs
@@ -1,5 +1,51 @@
 // [[file:~/Workspace/Programming/gosh/gosh.note::0f081329-2da0-4cd6-b622-265614169d68][0f081329-2da0-4cd6-b622-265614169d68]]
 use super::*;
 
+type Point3D = [f64; 3];
+
+pub trait Optimizer {
+    /// test if optimization converged
+    fn converged(&self, displacements: &Vec<Point3D>, mr: &ModelResults) -> bool;
+
+    /// Return the model for optimization
+    fn model_results(&self) -> Result<ModelResults>;
+
+    /// Return the max allowed iterations
+    fn max_iter(&self) -> usize {
+        1000
+    }
+
+    fn set_displacements(&mut self, displacements: &Vec<Point3D>);
+
+    /// carry out optimization
+    fn run(&mut self) -> Result<()> {
+        let n = self.max_iter();
+        let mut icycle = 0;
+        loop {
+            // calculate energy, forces, ... using model
+            let mr = self.model_results()?;
+            // calculate displacement vectors using optimizer
+            let dvs = self.displacements(&mr)?;
+            let forces = &mr.forces;
+            if self.converged(&dvs, &mr) {
+                break;
+            }
+
+            // update positions if not converged
+            self.set_displacements(&dvs);
+
+            icycle += 1;
+        }
+        if icycle >= n {
+            warn!("opt convergence failed!");
+        }
+
+        Ok(())
+    }
+
+    /// return displacement vectors predicted by the optimizer
+    fn displacements(&self, mr: &ModelResults) -> Result<Vec<Point3D>>;
+}
+
 pub mod fire;
 // 0f081329-2da0-4cd6-b622-265614169d68 ends here
