a812e8beb6225b99a16af5b100e5bb1b81234a12
Wenping Guo <ybyygu@gmail.com>
2018-09-20 11:32:02 +0800

    add a general interface for parsing results from text file
    based on nom parser combinator

--- src/adaptors/mod.rs
+++ src/adaptors/mod.rs
@@ -3,12 +3,15 @@
 // :header-args: :comments org :tangle src/adaptors/mod.rs
 // :END:
 
+use std::io::{Read, BufRead, BufReader};
 use std::path::Path;
 use std::str::Lines;
-use models::*;
+
 use quicli::prelude::*;
+use nom::{self, IResult};
 
 use gchemol::io;
+use models::*;
 
 pub trait ModelAdaptor {
     /// Parse calculated properties from output file.
@@ -36,6 +39,112 @@ pub trait ModelAdaptor {
     ///
     /// Return a list of ModelProperties
     fn parse_all(&self, output: &str) -> Result<Vec<ModelProperties>>;
+}
+
+pub struct TextParser {
+    /// The buffer size counted in number of lines
+    nlines: usize,
+}
+
+impl Default for TextParser {
+    fn default() -> Self {
+        TextParser {
+            nlines: 1000,
+        }
+    }
+}
+
+// the type for the parsed part
+type Part<'a> =  ModelProperties;
+
+impl TextParser {
+    fn parse<R: Read, F, C>(&self, f: R, parser: F, mut collector: C) -> Result<()>
+    where
+        F: Fn(&str) -> IResult<&str, Part>,
+        C: FnMut(Part),
+    {
+        // a. prepare data
+        let mut reader = BufReader::new(f);
+        let mut chunk = String::new();
+
+        // b. process the read/parse loop
+        // indicate if we finish reading
+        let mut eof = false;
+        'out: loop {
+            // 0. fill chunk
+            if ! eof {
+                debug!("fill data");
+                for _ in 0..self.nlines {
+                    // reach EOF
+                    if reader.read_line(&mut chunk)? <= 0 {
+                        eof = true;
+                        break;
+                    }
+                }
+            }
+
+            // 1. parse/consume the chunk until we get Incomplete error
+            // remained: the unprocessed lines by parser
+            let mut remained = String::new();
+            loop {
+                match parser(&chunk) {
+                    // 1.1 success parsed one part
+                    Ok((rest, part)) => {
+                        // save the remained data
+                        remained = String::from(rest);
+                        // collect the parsed value
+                        collector(part);
+                    },
+
+                    // 1.2 the chunk is incomplete.
+                    // `Incomplete` means the nom parser does not have enough data to decide,
+                    // so we wait for the next refill and then retry parsing
+                    Err(nom::Err::Incomplete(_)) => {
+                        // the chunk is unstained, so just break the parsing loop
+                        debug!("incomplete");
+                        break;
+                    },
+
+                    // 1.3 found parse errors, just ignore it and continue
+                    Err(nom::Err::Error(err)) => {
+                        // println!("the context lines: {}", chunk);
+                        // eprintln!("found parsing error: {:?}", err);
+                        eprintln!("found parsing error");
+                        break;
+                        // break 'out;
+                    },
+
+                    // 1.4 found serious errors
+                    Err(nom::Err::Failure(err)) => {
+                        bail!("encount hard failure: {:?}", err);
+                    },
+
+                    // 1.5 alerting nom changes
+                    _ => {
+                        bail!("found unrecovered nom state!");
+                    }
+                }
+
+                // 2. update the chunk with remained data after a successful parsing
+                // chunk.clear();
+                // chunk.push_str(&remained);
+                chunk = remained;
+            }
+
+            // all done, get out the loop
+            if eof {
+                if chunk.len() != 0 {
+                    warn!("remained data:\n {:}", chunk);
+                }
+                break
+            };
+
+        }
+        info!("parsing done.");
+
+        // c. finish the job
+        Ok(())
+    }
 }
 
 mod mopac;
--- src/adaptors/mopac.rs
+++ src/adaptors/mopac.rs
@@ -144,6 +144,167 @@ fn get_tagged_line(lines: &mut Lines, tag: &str) -> Option<String> {
     None
 }
 
+// nom
+
+use gchemol::parser::*;
+
+/// whitespace including one or more spaces or tabs
+named!(space_token<&str, &str>, eat_separator!(&b" \t"[..]));
+macro_rules! sp (
+    ($i:expr, $($args:tt)*) => (
+        {
+            sep!($i, space_token, $($args)*)
+        }
+    )
+);
+
+//  **                                MOPAC2016                                  **
+named!(mopac_header<&str, (&str, &str)>, do_parse!(
+       sp!(tag!("**"))    >>
+    m: sp!(tag!("MOPAC")) >>
+    v: digit              >>
+       sp!(tag!("**"))    >>
+    (
+        (m, v)
+    )
+));
+
+#[test]
+fn test_mopac_header() {
+    let line = " **                                MOPAC2016                                  **";
+    let (r, (m, v)) = mopac_header(line).unwrap();
+    assert!(r == "");
+    assert!(m == "MOPAC");
+    assert!(v == "2016");
+}
+
+
+//           TOTAL ENERGY            =       -720.18428 EV
+named!(total_energy<&str, f64>, do_parse!(
+            sp!(tag!("TOTAL ENERGY")) >>
+            sp!(tag!("="))            >>
+    energy: sp!(double_s)             >>
+            sp!(tag!("EV"))           >>
+    (energy)
+));
+
+#[test]
+fn test_mopac_energy() {
+    let line = "          TOTAL ENERGY            =       -720.18428 EV";
+    let (r, en) = total_energy(line).unwrap();
+    assert!(r == "");
+    assert_relative_eq!(-720.18428, en, epsilon=1e-4);
+}
+
+//  DIPOLE           X         Y         Z       TOTAL
+//  POINT-CHG.    -0.521    -0.058     0.081     0.531
+//  HYBRID        -0.027    -0.069    -0.010     0.075
+//  SUM           -0.548    -0.127     0.071     0.567
+named!(dipole<&str, [f64; 3]>, do_parse!(
+        sp!(tag!("DIPOLE")) >> read_until_eol >>
+        read_until_eol      >>
+        read_until_eol      >>
+        sp!(tag!("SUM"))    >>
+    x:  sp!(double_s)       >>
+    y:  sp!(double_s)       >>
+    z:  sp!(double_s)       >>
+        read_until_eol      >>
+
+    (
+        [x, y, z]
+    )
+));
+
+#[test]
+fn test_mopac_dipole() {
+    let txt = " DIPOLE           X         Y         Z       TOTAL
+ POINT-CHG.    -0.521    -0.058     0.081     0.531
+ HYBRID        -0.027    -0.069    -0.010     0.075
+ SUM           -0.548    -0.127     0.071     0.567
+";
+    let (r, [x, y, z]) = dipole(txt).unwrap();
+    assert_eq!(-0.548, x);
+    assert_eq!(-0.127, y);
+    assert_eq!(0.071, z);
+}
+
+//        FINAL  POINT  AND  DERIVATIVES
+//
+// PARAMETER     ATOM    TYPE            VALUE       GRADIENT
+//     1          1  C    CARTESIAN X    -1.644300   -55.598091  KCAL/ANGSTROM
+//     2          1  C    CARTESIAN Y    -0.817800    35.571574  KCAL/ANGSTROM
+//     3          1  C    CARTESIAN Z     0.125500   -22.556128  KCAL/ANGSTROM
+//     4          2  C    CARTESIAN X     1.631900     2.353930  KCAL/ANGSTROM
+//     5          2  C    CARTESIAN Y    -0.872000    -7.974745  KCAL/ANGSTROM
+//     6          2  C    CARTESIAN Z    -0.127300    12.066852  KCAL/ANGSTROM
+
+named!(sym_position_gradient<&str, (&str, f64, f64)>, do_parse!(
+              sp!(digit)                               >>
+              sp!(digit)                               >>
+    sym     : sp!(alpha)                               >>
+              sp!(tag!("CARTESIAN"))                   >>
+              sp!(alt!(tag!("X")|tag!("Y")|tag!("Z"))) >>
+    position: sp!(double_s)                            >>
+    gradient: sp!(double_s)                            >>
+              read_until_eol                           >>
+    (
+        (sym, position, gradient)
+    )
+));
+
+#[test]
+fn test_sym_position_gradient() {
+    let line = "      4          2  C    CARTESIAN X     1.523300     6.893093  KCAL/ANGSTROM\n";
+    let (_, (sym, position, gradient)) = sym_position_gradient(line).unwrap();
+    assert_eq!("C", sym);
+    assert_eq!(1.523300, position);
+    assert_eq!(6.893093, gradient);
+}
+
+named!(get_atom<&str, Atom>, do_parse!(
+    px: sym_position_gradient >>
+    py: sym_position_gradient >>
+    pz: sym_position_gradient >>
+    (
+        Atom::build()
+            .symbol(px.0)
+            .position(px.1, py.1, pz.1)
+            .momentum(px.2, py.2, pz.2)
+            .finish()
+    )
+));
+
+named!(get_atoms<&str, Vec<Atom>>, do_parse!(
+           sp!(tag!("PARAMETER")) >> read_until_eol >>
+    atoms: many1!(get_atom) >>
+
+    (
+        atoms
+    )
+));
+
+#[test]
+fn test_atoms() {
+    let txt = "   PARAMETER     ATOM    TYPE            VALUE       GRADIENT
+      1          1  C    CARTESIAN X    -1.743000   -80.695675  KCAL/ANGSTROM
+      2          1  C    CARTESIAN Y    -0.725100    73.306387  KCAL/ANGSTROM
+      3          1  C    CARTESIAN Z     0.044900   -23.565223  KCAL/ANGSTROM
+      4          2  C    CARTESIAN X     1.523300     6.893093  KCAL/ANGSTROM
+      5          2  C    CARTESIAN Y    -0.946300   -16.682683  KCAL/ANGSTROM
+      6          2  C    CARTESIAN Z    -0.005100    22.532087  KCAL/ANGSTROM
+      7          3  C    CARTESIAN X    -1.248600   -12.624765  KCAL/ANGSTROM
+      8          3  C    CARTESIAN Y     0.589400   -35.843890  KCAL/ANGSTROM
+      9          3  C    CARTESIAN Z    -0.026800     1.107735  KCAL/ANGSTROM
+     10          4  C    CARTESIAN X     1.222600   -40.743520  KCAL/ANGSTROM
+     11          4  C    CARTESIAN Y     0.386900    34.401001  KCAL/ANGSTROM
+     12          4  C    CARTESIAN Z     0.076200    -5.837845  KCAL/ANGSTROM\n\n";
+
+    let (_, atoms) = get_atoms(txt).unwrap();
+    assert_eq!(4, atoms.len());
+    assert_eq!(atoms[0].momentum(), [-80.695675, 73.306387, -23.565223]);
+    assert_eq!(atoms[0].position(), [-1.7430, -0.7251, 0.0449]);
+}
+
 // test
 
 #[test]
