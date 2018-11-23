9020463463f9df01cb5560cd8299f1dcdba2e1c3
Wenping Guo <ybyygu@gmail.com>
2018-11-23 17:16:48 +0800

    add strong wolfe test for line search

--- src/apps/optimization/line.rs
+++ src/apps/optimization/line.rs
@@ -7,9 +7,58 @@ pub struct Backtracking {
 
 impl Default for Backtracking {
     fn default() -> Self {
-        Backtracking {
-            epsilon: 1e-3,
+        Backtracking { epsilon: 1e-3 }
+    }
+}
+
+pub struct StrongWolfe {
+    /// Constant for the sufficient decrease condition.
+    c1: f64,
+    /// Constant for curvature condition.
+    c2: f64,
+}
+
+impl Default for StrongWolfe {
+    fn default() -> Self {
+        StrongWolfe { c1: 0.1, c2: 0.4 }
+    }
+}
+
+impl StrongWolfe {
+    /// Test inexact line search conditions are satisfied or not
+    /// # Parameters
+    /// - feval: closure to evaluate objective function and its first derivative
+    /// - xk, dk: current positions, search direction, respectively
+    pub fn satisfied(&self, xk: &[f64], dk: &[f64], alpha: f64, feval: F) -> bool
+    where
+        F: FnMut(&[f64], &mut [f64]) -> Result<f64>,
+    {
+        // sanity check
+        assert!(alpha.is_sign_positive());
+        assert!(self.c1 < self.c2);
+        let n = xk.len();
+        assert_eq!(n, dk.len());
+
+        // convert to dynamic vector/matrix (nalgebra)
+        let x_this = xk.to_dvector();
+        let direction = dk.to_dvector();
+        let mut g_this = vec![0.0; n];
+
+        // evaluate energy and gradients
+        let fx_this = feval(&x_this, &mut g_this);
+
+        let mut g_next = vec![0.0; n];
+        let x_next = x_this + alpha * direction;
+        let fx_next = feval(&x_next, &mut g_next);
+        // wolfe condition 1
+        if fx_this - fx_next >= - self.c1 * alpha * dk * g_this {
+            // wolfe condition 2
+            if (dk * g_next).abs() <= (self.c2 * dk * g_this).abs() {
+                true
+            }
         }
+
+        false
     }
 }
 // base:1 ends here
