3d869da55ca2743d394cb7507c5f9a507c801ce2
Wenping Guo <ybyygu@gmail.com>
2019-04-12 17:26:10 +0800

    fix Gaussian BBM template
    redirect stdout

--- templates/gaussian/submit.sh
+++ templates/gaussian/submit.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # read input stream from stdin
-g09 | tee gaussian.log 2> err.log
+g09 > gaussian.log 2> err.log
 
 # parsing results from formatted checkpoint file
 /share/apps/gaussian/bin/gaussian-adaptor Test.FChk
